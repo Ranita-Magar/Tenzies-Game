a76514fceaf678f1191cc65ca5c83b52eb1bd3a1
Ranita-Magar <87315393+Ranita-Magar@users.noreply.github.com>
2024-05-01 12:42:11 +0545

    Mapping Array To Die Components

--- App.jsx
+++ App.jsx
@@ -1,31 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Die } from "./Die";
 
 export const App = () => {
+  const [dice, setDice] = useState(allNewDice());
+
   function allNewDice() {
     const newDice = [];
-
     for (let i = 0; i < 10; i++) {
       newDice.push(Math.ceil(Math.random() * 6));
     }
     return newDice;
   }
-  console.log(allNewDice());
+
+  const diceElements = dice.map((die) => <Die value={die} />);
 
   return (
     <main>
-      <div className="dice-container">
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
-        <Die value="5" />
-        <Die value="6" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-      </div>
+      <div className="dice-container">{diceElements}</div>
     </main>
   );
 };
