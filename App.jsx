117d38eb213c3237e06c1ae318f5a78a75caafa5
Ranita-Magar <87315393+Ranita-Magar@users.noreply.github.com>
2024-05-01 12:25:56 +0545

    Generating an array of 10 random numbers

--- App.jsx
+++ App.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import { Die } from "./Die";
 
 export const App = () => {
+  function allNewDice() {
+    const newDice = [];
+
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+  console.log(allNewDice());
+
   return (
     <main>
       <div className="dice-container">
